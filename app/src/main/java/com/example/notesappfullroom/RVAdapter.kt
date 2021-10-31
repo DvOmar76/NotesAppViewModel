eafcb211c62367dbd4c6a6cc44aca00d00c9e95c
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2021-10-31 16:37:19 +0300

    ..

--- app/src/main/java/com/example/notesappfullroom/RVAdapter.kt
+++ app/src/main/java/com/example/notesappfullroom/RVAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notesappfullroom.DBRoom.Note
